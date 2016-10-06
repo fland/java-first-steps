1cb8077de9037df99a5e69973884fb890ae3979e
asya <anastasia.mordovec@gmail.com>
2016-10-06 22:56:44 +0200

    second commit: added  Main.java and Student.java

---
+++ school-repo/src/main/java/first/steps/school/repo/Main.java
@@ -0,0 +1,12 @@
+package first.steps.school.repo;
+
+/**
+ * @author Maksym Bondarenko
+ * @since 06/10/2016
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
