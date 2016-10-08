fa998e3d83adaed26fc77555ea2a8fd2b0aeb12e
asya <anastasia.mordovec@gmail.com>
2016-10-08 16:41:47 +0200

    added Student data structure

--- school-repo/src/main/java/first/steps/school/repo/model/Student.java
+++ school-repo/src/main/java/first/steps/school/repo/model/Student.java
@@ -1,10 +1,25 @@
 package first.steps.school.repo.model;
 
+import java.util.Date;
+
 /**
  * @author Maksym Bondarenko
  * @since 06/10/2016
  */
 public class Student {
+
+    private String firstName, lastName, className;
+    private Date birthDate;
+    private Double hight, weight;
+
+    public Student(String firstName, String lastName, String className, Date birthDate, Double hight, Double weight) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.className = className;
+        this.birthDate = birthDate;
+        this.hight = hight;
+        this.weight = weight;
+    }
 
 //    todo: put student data structure here
 }
