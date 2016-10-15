46e1efbad1c96f8592a27db904b1d6258f63d1bf
asya <anastasia.mordovec@gmail.com>
2016-10-15 19:06:56 +0200

    + unit tests for Group class improvement

--- school-repo/src/test/java/first/steps/school/repo/model/GroupTest.java
+++ school-repo/src/test/java/first/steps/school/repo/model/GroupTest.java
@@ -14,22 +14,40 @@ import static org.junit.Assert.*;
 public class GroupTest {
 
     @Test
-    public void testAddingStudent() {
-        List<Student> students1 = new ArrayList<Student>();
-        students1.add(new Student("A", "B", "1C", new Date(), 55.f, 170));
-        Group myGroup = new Group();
-        myGroup.addStudent(new Student("A", "B", "1C", new Date(), 55.f, 170));
-        assertEquals(students1.size(), myGroup.getStudents().size());
+    public void testAddStudentOneStudent() {
+        Group group = new Group();
+        assertTrue(group.getStudents().isEmpty());
+        Student expectedStudent = new Student("A", "B", "1C", new Date(), 55.f, 170);
+        group.addStudent(expectedStudent);
+        assertTrue(group.getStudents().contains(expectedStudent));
+        assertEquals(1, group.getStudents().size());
+    }
+
+    @Test
+    public void testAddStudentSeveralStudents() {
+        Group group = new Group();
+        assertTrue(group.getStudents().isEmpty());
+        List<Student> expectedStudents = new ArrayList<Student>();
+        Student student = new Student("A", "B", "1C", new Date(), 55.f, 170);
+        group.addStudent(student);
+        expectedStudents.add(student);
+        student = new Student("1A", "1B", "2C", new Date(), 33.f, 150);
+        group.addStudent(student);
+        expectedStudents.add(student);
+        student = new Student("2A", "2B", "3C", new Date(), 44.f, 180);
+        group.addStudent(student);
+        expectedStudents.add(student);
+        assertTrue(group.getStudents().containsAll(expectedStudents));
+        assertTrue(expectedStudents.containsAll(group.getStudents()));
     }
 
 
     @Test
-    public void testAddStudentValidationForNullValue() {
-        List<Student> students1 = new ArrayList<Student>();
-        students1.add(new Student("A", "B", "1C", new Date(), 55.f, 170));
-        Group myGroup = new Group();
-        myGroup.addStudent(null);
-        assertNotEquals(students1.size(), myGroup.getStudents().size());
+    public void testAddStudentAddingNullValue() {
+        Group group = new Group();
+        assertTrue(group.getStudents().isEmpty());
+        group.addStudent(null);
+        assertTrue(group.getStudents().isEmpty());
     }
 
     @Test
@@ -44,7 +62,7 @@ public class GroupTest {
 
     @Test
     public void deleteStudentByIndex() {
-        
+
 
     }
 
